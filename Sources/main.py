7423f69672355ba7aedca6538eb9ae7d81210fac
AtthaphanPak <atthaphanpaksakunnee@gmail.com>
2025-08-07 13:31:06 +0700

    add setting_window

--- Sources/main.py
+++ Sources/main.py
@@ -31,7 +31,7 @@ class MainWindow(QMainWindow):
             self.IL_IP = self.config["DEFAULT"].get("IL_IP", "")
             self.IL_PORT = int(self.config["DEFAULT"].get("IL_PORT", ""))
             self.LogPath = self.config["DEFAULT"].get("LogPath", "")
-            self.mode = self.config["DEFAULT"].get("MODE", "")
+            self.mode = self.config["DEFAULT"].get("FITS", "")
 
             self.model = self.config["FITs"].get("model", "")
             self.operation = self.config["FITs"].get("operation", "")
@@ -77,6 +77,8 @@ class MainWindow(QMainWindow):
         self.StartButton.clicked.connect(self.check_serials)
         self.LogoutButton.clicked.connect(self.logout)
         self.FinishButton.clicked.connect(self.finish_cycle)
+        self.buttonBox.rejected.connect(self.back_to_main)
+        self.buttonBox.accepted.connect(self.save_setting)
 
     def closeEvent(self, event):
         print("CloseEvent")
@@ -116,13 +118,101 @@ class MainWindow(QMainWindow):
     def select_mode(self):
         password, ok = QInputDialog.getText(self, "Admin login", "Enter admin password:", QLineEdit.EchoMode.Password)
         if ok and password == "Admin123":
-            mode_dialog = QInputDialog()
-            mode, ok2 = QInputDialog.getItem(self, "Select Mode", "Choose mode:",["Production", "Debug"], 0, False)
-            if ok2:
-                self.mode = mode
+            self.MainstackedWidget.setCurrentIndex(4)
+            self.setting_window()
         else:
             QMessageBox.warning(self, "Asscess Denied", "Incorrect password.")
 
+    def setting_window(self):
+        if self.mode == "True":
+            self.FITs.setCurrentText("True")
+        else:
+            self.FITs.setCurrentText("False")
+
+        self.Log_Path.setText(self.LogPath)
+        self.Model_edit.setText(self.model)
+        self.opn.setText(self.operation)
+        self.mean.setText(str(self.spec["limit"]))
+        self.tolrance.setText(str(self.spec["tolerance"]))
+        self.ip.setText(self.IL_IP)
+        self.port.setText(str(self.IL_PORT))
+        self.cover_1_X.setText(str(self.mesure_points[0][0]))
+        self.cover_2_X.setText(str(self.mesure_points[1][0]))
+        self.cover_3_X.setText(str(self.mesure_points[2][0]))
+        self.Bench_1_X.setText(str(self.mesure_points[3][0]))
+        self.Bench_2_X.setText(str(self.mesure_points[4][0]))
+        self.Bench_3_X.setText(str(self.mesure_points[5][0]))
+        self.Bench_4_X.setText(str(self.mesure_points[6][0]))
+        self.cover_1_Y.setText(str(self.mesure_points[0][1]))
+        self.cover_2_Y.setText(str(self.mesure_points[1][1]))
+        self.cover_3_Y.setText(str(self.mesure_points[2][1]))
+        self.Bench_1_Y.setText(str(self.mesure_points[3][1]))
+        self.Bench_2_Y.setText(str(self.mesure_points[4][1]))
+        self.Bench_3_Y.setText(str(self.mesure_points[5][1]))
+        self.Bench_4_Y.setText(str(self.mesure_points[6][1]))
+
+    def back_to_main(self):
+        self.MainstackedWidget.setCurrentIndex(0)
+
+    def save_setting(self):
+        self.config.set("DEFAULT", "LogPath", self.Log_Path.text())
+        self.config.set("DEFAULT", "FITS", self.FITs.currentText())
+        self.config.set("DEFAULT", "IL_IP", self.ip.text())
+        self.config.set("DEFAULT", "IL_PORT", self.port.text())
+        self.config.set("FITs", "model", self.Model_edit.text())
+        self.config.set("FITs", "operation", self.opn.text())
+        self.config.set("SPEC", "limit", self.mean.text())
+        self.config.set("SPEC", "tolerance", self.tolrance.text())
+        self.config.set("SPEC", "cover1_x", self.cover_1_X.text())
+        self.config.set("SPEC", "cover2_x", self.cover_2_X.text())
+        self.config.set("SPEC", "cover3_x", self.cover_3_X.text())
+        self.config.set("SPEC", "Bench1_x", self.Bench_1_X.text())
+        self.config.set("SPEC", "Bench2_x", self.Bench_2_X.text())
+        self.config.set("SPEC", "Bench3_x", self.Bench_3_X.text())
+        self.config.set("SPEC", "Bench4_x", self.Bench_4_X.text())
+        self.config.set("SPEC", "cover1_y", self.cover_1_Y.text())
+        self.config.set("SPEC", "cover2_y", self.cover_2_Y.text())
+        self.config.set("SPEC", "cover3_y", self.cover_3_Y.text())
+        self.config.set("SPEC", "bench1_y", self.Bench_1_Y.text())
+        self.config.set("SPEC", "bench2_y", self.Bench_2_Y.text())
+        self.config.set("SPEC", "bench3_y", self.Bench_3_Y.text())
+        self.config.set("SPEC", "bench4_y", self.Bench_4_Y.text())
+
+        with open("C:\Projects\Cells_DR\Properties\Config.ini", "w") as configfile:
+            self.config.write(configfile)
+        
+        self.reload_config()
+
+    def reload_config(self):
+        self.config.read("C:\Projects\Cells_DR\Properties\Config.ini")
+        try:
+            self.config.read("C:\Projects\Cells_DR\Properties\Config.ini")
+            self.IL_IP = self.config["DEFAULT"].get("IL_IP", "")
+            self.IL_PORT = int(self.config["DEFAULT"].get("IL_PORT", ""))
+            self.LogPath = self.config["DEFAULT"].get("LogPath", "")
+            self.mode = self.config["DEFAULT"].get("FITS", "")
+
+            self.model = self.config["FITs"].get("model", "")
+            self.operation = self.config["FITs"].get("operation", "")
+            self.spec = {
+                "limit": float(self.config["SPEC"].get("limit", "")),
+                "tolerance": float(self.config["SPEC"].get("tolerance", ""))
+            }
+            for i in range(1, 4):
+                x = float(self.config["SPEC"].get(f"cover{i}_X"))
+                y = float(self.config["SPEC"].get(f"cover{i}_Y"))
+                self.mesure_points.append([x, y])
+            for i in range(1, 5):
+                x = float(self.config["SPEC"].get(f"bench{i}_X"))
+                y = float(self.config["SPEC"].get(f"bench{i}_Y"))
+                self.mesure_points.append([x, y])
+            # print(self.mesure_points)
+
+        except Exception as e:
+            print(f"{e}\nPlease check config.ini")
+            print("Close Program", f"{e}\nPlease check config.ini")
+            quit()
+        
     def login(self):
         self.en = self.enLineEdit.text()
         if len(self.en) == 6:
@@ -147,7 +237,7 @@ class MainWindow(QMainWindow):
         print("check_serials")
         self.sn_cover = self.SNCoverValue.text()
         self.sn_bench = self.SNBenchValue.text()
-        if self.mode.upper() == "PRODUCTION":
+        if self.mode.upper() == "True":
             if len(self.sn_cover) == 12:
                 fits_status = fn_Handshake(self.model, self.operation, self.sn_cover)
                 if fits_status == True:
@@ -264,7 +354,7 @@ class MainWindow(QMainWindow):
                 
             parameters = ";".join(self.df.keys())
             values = ";".join(self.df.values())
-        if self.mode.upper() == "PRODUCTION":
+        if self.mode.upper() == "True":
             fits_status = fn_Log(self.model, self.operation, parameters, values)
             if fits_status == True:
                 QMessageBox.information(self, "FITs Message", "Data has been uploaded to FITs")
